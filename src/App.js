d086811b8bd5d237ee1dfe74604d595513ef334d
akshay98322 <akshay98322@gmail.com>
2022-04-07 17:08:52 +0530

    Disappering Alert added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,39 @@ import './App.css';
 import React,{ useState } from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
+import Alert from './components/Alert';
 
 
 function App() {
-  const [mode, setMode] = useState(false);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type})
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000)
+  }
+  const togleMode = () => {
+    if (mode === "light"){
+        setMode("dark");
+        document.body.style.backgroundColor = "#ddd";
+        showAlert("Dark Mode Enabled", "success")
+    } else {
+        setMode("light");
+        document.body.style.backgroundColor = "white";
+        showAlert("Light Mode Enabled", "success")
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About" mode={mode}/>
+    <Navbar title="TextUtils" aboutText="About" mode={mode} togleMode={togleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analize" />
+      <TextForm heading="Enter the text to analize" mode={mode} showAlert={showAlert}/>
       {/* <About/> */}
     </div>    
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,34 +7,40 @@ export default function TextForm(props) {
     const handleUpClick = () =>{
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Text converted to Uppercase", 'success')
     }
     // conver the text to lowercase
     const handleLoClick = () =>{
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Text converted to Lowercase", 'success')
     }
     // Clear Text
     const handleClClick = () =>{
         let newText = "";
         setText(newText);
+        props.showAlert("Text cleared", 'success')
     }
     // Capatilize Text
     const handleCapClick = () =>{
         if (text !== ""){
             let newText = text[0].toUpperCase() + text.substring(1).toLowerCase();
             setText(newText);
+            props.showAlert("Text capitalized", 'success')
         } else {
-            alert("Please enter text first");
+            props.showAlert("Please enter text first", 'danger')
         }
     }
     // Copy Text
     const handleCopy= () =>{
         navigator.clipboard.writeText(text);
+        props.showAlert("text copied to clipboard.", 'success')
     }
     // Remove extra spaces
     const handleRemoveSpace = ()=>{
         let newText = text.split(/[ ]+/).join(" ");
         setText(newText);
+        props.showAlert("Extra spaces removed.", 'success')
     }
 
     // to handle change in TextArea
@@ -47,9 +53,10 @@ export default function TextForm(props) {
     // Rendering part
     return (
         <>
-        <div className='container'>
+        <div className='container' >
             <h1>{props.heading}</h1>
             <div className="mb-3">
+                {/* rgb(21,32,43)!importan */}
                 <textarea className="form-control" placeholder={placeholder} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>                
             </div>   
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Upper</button>
@@ -57,14 +64,14 @@ export default function TextForm(props) {
             <button className="btn btn-success mx-1" onClick={handleLoClick}>Lower</button>
             <button className="btn btn-danger mx-1" onClick={handleClClick}>Clear</button>            
             <button className="btn btn-warning mx-1" onClick={handleCopy}>Copy</button>
-            <button className="btn btn-info mx-1" onClick={handleRemoveSpace}>Revove Space</button>       
+            <button className="btn btn-info mx-1" onClick={handleRemoveSpace}>Remove Space</button>       
         </div>
         <div className="container my-3">
             <h2>Your Text summary</h2>
             <p>Your text has {text.split(" ").length} words and {text.length} characters.</p>
             <p>It will take {text.split(" ").length *0.008 } minutes to read</p>
             <h4>Text Preview</h4>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the the textbox to view here"}</p>
             
 
         </div>
